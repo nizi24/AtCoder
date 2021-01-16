47e4afe57ab515dba514066a3ed0b26b52195f71
nizi <failaforcontact@gmail.com>
2021-01-16 20:28:32 +0900

    APG4b-EX10を追加

---
+++ APG4b/EX10.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  int ai = 0, bi = 0;
+  
+  cout << "A:";
+  while (ai < A) {
+    cout << "]";
+  	ai++;
+  }
+  cout << endl;
+  
+  cout << "B:";
+  while (bi < B) {
+   	cout << "]";
+    bi++;
+  }
+  cout << endl;
+}
