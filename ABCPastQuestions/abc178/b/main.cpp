37eb12a81b183bc26bb88d7f416ed459f4022461
nizi <failaforcontact@gmail.com>
2021-01-19 13:04:50 +0900

    abc178/bを追加

---
+++ ABCPastQuestions/abc178/b/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+
+void solve(long long a, long long b, long long c, long long d){
+    cout << max(max(a*c, a*d), max(b*c, b*d)) << endl;
+}
+
+int main(){
+    long long a;
+    scanf("%lld",&a);
+    long long b;
+    scanf("%lld",&b);
+    long long c;
+    scanf("%lld",&c);
+    long long d;
+    scanf("%lld",&d);
+    solve(a, b, c, d);
+    return 0;
+}
