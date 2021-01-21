7d9b31c2c1e978b7613ecdb7479b42cb77d98bfc
nizi <failaforcontact@gmail.com>
2021-01-21 10:11:53 +0900

    abc081/bを追加

---
+++ ABCPastQuestions/abc081/b/main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+const long long MOD = 2;
+
+void solve(long long N, std::vector<long long> A){
+	int min = INT16_MAX;
+	REP (i, N) {
+		int cnt = 0;
+		while (A.at(i) % 2 == 0) {
+			A.at(i) /= 2;
+			cnt++;
+		}
+		if (min > cnt) min = cnt;
+	}
+	cout << min << endl;
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> A(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&A[i]);
+    }
+    solve(N, std::move(A));
+    return 0;
+}
