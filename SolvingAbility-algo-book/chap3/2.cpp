dea315359b9fa28c03868fbe4688e74191d8b38c
nizi <failaforcontact@gmail.com>
2021-01-21 09:32:36 +0900

    3-2を追加

---
+++ SolvingAbility-algo-book/chap3/2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, v;
+    cin >> N >> v;
+    vector<int> n(N);
+    for (int i = 0;i<N;i++) cin >> n.at(i);
+    int ans = 0;
+    for (int i = 0; i < N; i++) {
+        if (v == n.at(i)) ans++;
+    }
+}
