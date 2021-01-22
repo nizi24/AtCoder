26d7494dda06dab37f999c0220e968e118b2f6c3
nizi <failaforcontact@gmail.com>
2021-01-22 12:27:02 +0900

    EducationalDPContest/bを追加

--- EducationalDPContest/b.cpp
+++ EducationalDPContest/b.cpp
@@ -22,13 +22,13 @@ inline bool chmax(T &a, T b) {
 int main() {
     int n, k;
     cin >> n >> k;
-    vector<int> h(n);
-    REP(i, n) cin >> h.at(i);
-    vector<int> dp(n+1, INF);
+    vector<int> h(n+1000);
+    REP(i, n) cin >> h[i];
+    vector<int> dp(n+1000, INF);
     dp[0] = 0;
     REP(i, n) {
         for (int j = 1; j <= k; j++) {
-            chmin(dp[i+j], abs(h[i] - h[i+j]) + dp[i]);
+            chmin(dp[i+j], dp[i] + abs(h[i] - h[i+j]));
         }
     }
     cout << dp[n-1] << endl;
