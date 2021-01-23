ba183aec262de215a49d3db94c1770b76c5f4517
nizi <failaforcontact@gmail.com>
2021-01-23 23:32:44 +0900

    abc189/aを追加

---
+++ ABCPastQuestions/abc189/a/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+
+
+int main(){
+	string C;
+	cin >> C;
+	string ans = "Lost";
+	if (C.at(0) == C.at(1) && C.at(0) == C.at(2)) ans = "Won";
+	cout << ans << endl;
+    return 0;
+}
