368f94d27444d1a76020b2d2d647028d0c536a3e
nizi <failaforcontact@gmail.com>
2021-01-24 21:19:42 +0900

    abc171/bを追加

---
+++ ABCPastQuestions/abc171/b/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+
+void solve(long long N, long long K, std::vector<long long> p){
+    ALL(sort, p);
+    int ans = 0;
+    REP (i, K) ans += p[i];
+    cout << ans << endl;
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    long long K;
+    scanf("%lld",&K);
+    std::vector<long long> p(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&p[i]);
+    }
+    solve(N, K, std::move(p));
+    return 0;
+}
