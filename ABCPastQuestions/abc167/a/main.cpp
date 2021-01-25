5e2abde7c2a441ea877c2bd94fea9f09e3aa323f
nizi <failaforcontact@gmail.com>
2021-01-26 08:25:06 +0900

    abc167/aを追加

---
+++ ABCPastQuestions/abc167/a/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+const string YES = "Yes";
+const string NO = "No";
+
+void solve(std::string S, std::string T){
+	if (S == T.substr(0, T.size() - 1)) cout << YES << endl;
+	else cout << NO << endl;
+}
+
+int main(){
+    std::string S;
+    std::cin >> S;
+    std::string T;
+    std::cin >> T;
+    solve(S, T);
+    return 0;
+}
