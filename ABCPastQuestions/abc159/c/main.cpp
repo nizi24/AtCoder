c79aa4766aaf130a02ae9932cdf408e9b332817c
nizi <failaforcontact@gmail.com>
2021-01-26 16:44:09 +0900

    abc159/cを追加

---
+++ ABCPastQuestions/abc159/c/main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template<class T>
+inline T sum(T n){return n*(n+1)/2;}
+
+
+void solve(long long L){
+	cout << std::fixed << std::setprecision(15) << L * L * L  / 27 << endl;
+}
+
+int main(){
+    long long L;
+    scanf("%lld",&L);
+    solve(L);
+    return 0;
+}
