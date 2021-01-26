b2494860b771a47a9458d0d24ee979a1b1e9eadd
nizi <failaforcontact@gmail.com>
2021-01-26 22:36:31 +0900

    abc153/bを追加

---
+++ ABCPastQuestions/abc153/b/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template<class T>
+inline T sum(T n){return n*(n+1)/2;}
+
+const string YES = "Yes";
+const string NO = "No";
+
+void solve(long long H, long long N, std::vector<long long> A){
+    ALL(sort, A);
+    for (int i = A.size()-1; i >= 0; i--) H -= A[i];
+    cout << (H <= 0 ? YES : NO) << endl;
+}
+
+int main(){
+    long long H;
+    scanf("%lld",&H);
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> A(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&A[i]);
+    }
+    solve(H, N, std::move(A));
+    return 0;
+}
