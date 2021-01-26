33a516e1bb56a998ef45130300e55aaa2e35a477
nizi <failaforcontact@gmail.com>
2021-01-26 17:06:15 +0900

    abc158/bを追加

---
+++ ABCPastQuestions/abc158/b/main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template<class T>
+inline T sum(T n){return n*(n+1)/2;}
+
+
+void solve(long long N, long long A, long long B){
+	long long ans = 0;
+	if (N <= A) ans = N;
+	else if (N <= A + B) ans = A;
+	else {
+		ans = N / (A + B) * A;
+		if (N % (A + B) >= A) ans += A;
+		else ans += N % (A + B);
+	}
+	cout << ans << endl;
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    long long A;
+    scanf("%lld",&A);
+    long long B;
+    scanf("%lld",&B);
+    solve(N, A, B);
+    return 0;
+}
