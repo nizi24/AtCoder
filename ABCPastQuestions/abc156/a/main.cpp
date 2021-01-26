638c97075686b6af1de3e06e7fe6efa68cae9c74
nizi <failaforcontact@gmail.com>
2021-01-26 20:48:13 +0900

    abc156/aを追加

---
+++ ABCPastQuestions/abc156/a/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template<class T>
+inline T sum(T n){return n*(n+1)/2;}
+
+
+void solve(long long N, long long R){
+	cout << (N >= 10 ? R : R + 100 * (10 - N)) << endl;
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    long long R;
+    scanf("%lld",&R);
+    solve(N, R);
+    return 0;
+}
