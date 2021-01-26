886c7ff73997e45a0e73946dfd98495a2c52a6d3
nizi <failaforcontact@gmail.com>
2021-01-26 10:09:28 +0900

    abc164/bを追加

---
+++ ABCPastQuestions/abc164/a/main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+
+void solve(long long S, long long W){
+	cout << (W >= S ? "unsafe" : "safe") << endl;
+}
+
+int main(){
+    long long S;
+    scanf("%lld",&S);
+    long long W;
+    scanf("%lld",&W);
+    solve(S, W);
+    return 0;
+}
