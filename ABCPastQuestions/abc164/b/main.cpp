c2c15d0f2e1d8cf0471cf2751f41f7ea5fb8e9a0
nizi <failaforcontact@gmail.com>
2021-01-26 10:17:05 +0900

    abc164/bを追加

---
+++ ABCPastQuestions/abc164/b/main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+const string YES = "Yes";
+const string NO = "No";
+
+void solve(long long A, long long B, long long C, long long D){
+    while (A > 0 && C > 0) {
+        A -= D;
+        C -= B;
+        if (C <= 0) cout << "Yes" << endl;
+        else if (A <= 0) cout << "No" << endl;
+    }
+}
+
+int main(){
+    long long A;
+    scanf("%lld",&A);
+    long long B;
+    scanf("%lld",&B);
+    long long C;
+    scanf("%lld",&C);
+    long long D;
+    scanf("%lld",&D);
+    solve(A, B, C, D);
+    return 0;
+}
