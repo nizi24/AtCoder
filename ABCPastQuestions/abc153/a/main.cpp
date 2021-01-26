4fe952ddbe5ddc35ef4a95fef05dc432d16c4a74
nizi <failaforcontact@gmail.com>
2021-01-26 22:32:53 +0900

    abc153/aをtuika

---
+++ ABCPastQuestions/abc153/a/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template<class T>
+inline T sum(T n){return n*(n+1)/2;}
+
+
+void solve(long long H, long long A){
+	cout << (H + A - 1) / A << endl;
+}
+
+int main(){
+    long long H;
+    scanf("%lld",&H);
+    long long A;
+    scanf("%lld",&A);
+    solve(H, A);
+    return 0;
+}
