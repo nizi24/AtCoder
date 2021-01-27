a6fc0c3907469f418f5f704a5353bc3627cdd154
nizi <failaforcontact@gmail.com>
2021-01-27 20:49:21 +0900

    templateを更新

--- templete.cpp
+++ templete.cpp
@@ -6,6 +6,7 @@ using ll = long long;
 #define INF INT32_MAX / 2
 #define REP(i, n) for (int i = 0; i < n; i++)
 #define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
 template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
 template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
 template<class T>inline T sum(T n){return n*(n+1)/2;}
