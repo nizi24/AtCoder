4bd6efecf396cefe07ce211b32772aecf654d9cd
nizi <failaforcontact@gmail.com>
2021-01-27 17:45:22 +0900

    abc140/aを追加

---
+++ ABCPastQuestions/abc140/a/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(int i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(int i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+
+
+void solve(long long N){
+    cout << N * N * N << endl;
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    solve(N);
+    return 0;
+}
