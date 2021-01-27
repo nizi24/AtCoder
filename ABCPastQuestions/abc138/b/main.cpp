ef01f0fb1b3741a42850a80e1b5c01f2e3c69013
nizi <failaforcontact@gmail.com>
2021-01-27 22:07:37 +0900

    abc138/bを追加

--- ABCPastQuestions/abc138/b/main.cpp
+++ ABCPastQuestions/abc138/b/main.cpp
@@ -7,7 +7,6 @@ using ll = long long;
 #define REP(i, n) for (int i = 0; i < n; i++)
 #define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
 #define c(n) cout<<n<<endl;
-#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
 template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
 template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
 template<class T>inline T sum(T n){return n*(n+1)/2;}
@@ -15,28 +14,20 @@ map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(int i=2;i*i<=A;i++){while(A%i== 0){
 bool is_prime(ll N){if(N<=1)return false;for(int i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
 template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
 
-{% if mod %}
-const long long MOD = {{ mod }};
-{% endif %}
-{% if yes_str %}
-const string YES = "{{ yes_str }}";
-{% endif %}
-{% if no_str %}
-const string NO = "{{ no_str }}";
-{% endif %}
 
-{% if prediction_success %}
-void solve({{ formal_arguments }}){
-
+void solve(long long N, std::vector<long long> A){
+    double ans = 0;
+    REP (i, N) ans += 1.0 / A[i];
+    c(1 / ans)
 }
-{% endif %}
 
 int main(){
-    {% if prediction_success %}
-    {{input_part}}
-    solve({{ actual_arguments }});
-    {% else %}
-    // Failed to predict input format
-    {% endif %}
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> A(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&A[i]);
+    }
+    solve(N, std::move(A));
     return 0;
 }
