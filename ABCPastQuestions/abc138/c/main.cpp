d2de15ca41496b85a8391be4064550f75eac75cf
nizi <failaforcontact@gmail.com>
2021-01-27 22:21:08 +0900

    abc138/cを追加

---
+++ ABCPastQuestions/abc138/c/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(int i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(int i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+
+
+void solve(long long N, std::vector<long long> v){
+    ALL(sort, v);
+    stack<double> stc;
+    int size = v.size();
+    REP (i, N) stc.push(v[size - 1 - i]);
+    while (stc.size() > 1) {
+        double v1 = stc.top();
+        stc.pop();
+        double v2 = stc.top();
+        stc.pop();
+        stc.push((v1 + v2) / 2.0);
+    }
+    c(stc.top())
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> v(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&v[i]);
+    }
+    solve(N, std::move(v));
+    return 0;
+}
