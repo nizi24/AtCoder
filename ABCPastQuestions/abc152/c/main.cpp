f179067b3404bea31e77de6e507e5c721478ff6f
nizi <failaforcontact@gmail.com>
2021-01-27 09:45:35 +0900

    abc152/cを追加

---
+++ ABCPastQuestions/abc152/c/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template<class T>
+inline T sum(T n){return n*(n+1)/2;}
+
+
+void solve(long long N, std::vector<long long> P){
+	long long min = INF;
+	int ans = 0;
+	REP (i, N) if (chmin(min, P[i])) ans++;
+	cout << ans << endl;
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> P(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&P[i]);
+    }
+    solve(N, std::move(P));
+    return 0;
+}
