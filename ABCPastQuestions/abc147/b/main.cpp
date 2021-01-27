43d421539c593531b38892b77efea3731814e93d
nizi <failaforcontact@gmail.com>
2021-01-27 12:30:11 +0900

    abc147/bを追加

---
+++ ABCPastQuestions/abc147/b/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(int i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(int i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+
+
+void solve(std::string S){
+    int ans = 0;
+    string a = S.substr(0, S.size()/2);
+    string b = S.substr((S.size() + 1) /2);
+    ALL(reverse, b);
+    REP (i, a.size()) if (a[i] != b[i]) ans++;
+    cout << ans << endl;
+}
+
+int main(){
+    std::string S;
+    std::cin >> S;
+    solve(S);
+    return 0;
+}
