060d4335c59d2251b398055ba95d5c6b89e951b5
nizi <failaforcontact@gmail.com>
2021-01-28 10:56:36 +0900

    abc133/cを追加

---
+++ ABCPastQuestions/abc133/c/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(int i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(int i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+
+const long long MD = 2019;
+
+void solve(long long L, long long R){
+    ll min = 2019;
+    for (int i = L; i <= R; i++) {
+        bool bre = false;
+        if (i % 2019 == 0) {
+            min = 0;
+            break;
+        }
+        for (int j = i + 1; j <= R; j++) {
+            if (j % 2019 == 0) {
+                min = 0;
+                break;
+            }
+            chmin(min, ((i % MD) * (j % MD)) % MD);
+            if (min == 0) {
+                break;
+                bre = true;
+            }
+        }
+        if (bre) break;
+    }
+    c(min)
+}
+
+int main(){
+    long long L;
+    scanf("%lld",&L);
+    long long R;
+    scanf("%lld",&R);
+    solve(L, R);
+    return 0;
+}
