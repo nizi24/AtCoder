f28ca6a90c1b38751cfe94429d0f436cac4e78d4
nizi <failaforcontact@gmail.com>
2021-01-28 23:07:43 +0900

    abc129/bを追加

---
+++ ABCPastQuestions/abc129/b/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(int i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(int i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+
+
+void solve(long long N, std::vector<long long> W){
+    int min = INF;
+    for (int i = 1; i < N-1; i++) {
+        int left = 0;
+        int right = 0;
+        for (int j = 0; j < i + 1; j++) left += W[j];
+        for (int j = i + 1; j < N; j++) right += W[j];
+        chmin(min, abs(left - right));
+    }
+    c(min)
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> W(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&W[i]);
+    }
+    solve(N, std::move(W));
+    return 0;
+}
