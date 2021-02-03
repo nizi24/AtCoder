a7ad81c05302cf123941443a38d18a188c3c0985
nizi <failaforcontact@gmail.com>
2021-02-03 16:58:15 +0900

    abc173/dを追加

---
+++ ABCPastQuestions/abc173/d/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+
+void solve(long long N, std::vector<long long> A){
+	long long ans = 0;
+	queue<long long> feel;
+	sort(A.begin(), A.end(), greater<long long>());
+	REP (i, N) {
+		if (i == 0) feel.push(A[i]);
+		else {
+			ans += feel.front();
+			feel.pop();
+			feel.push(A[i]);
+			feel.push(A[i]);
+		}
+	}
+	cout << ans << endl;
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> A(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&A[i]);
+    }
+    solve(N, std::move(A));
+    return 0;
+}
