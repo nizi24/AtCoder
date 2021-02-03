a0fb7446e751137c70d9744d9ed22be41543b65c
nizi <failaforcontact@gmail.com>
2021-02-03 12:16:57 +0900

    abc159/dを追加

---
+++ ABCPastQuestions/abc159/d/main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template<class T>
+inline T sum(T n){return n*(n+1)/2;}
+
+
+void solve(long long N, std::vector<long long> A){
+	vector<long long> backet(N+1, 0);
+	REP (i, N) backet[A[i]]++;
+	long long sm = 0;
+	REP (i, N+1) if (backet[i]) sm += sum(backet[i] - 1);
+	REP (i, N) {
+		long long SUM = sm;
+		SUM -= backet[A[i]] - 1;
+		cout << SUM << endl;
+	}
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> A(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&A[i]);
+    }
+    solve(N, std::move(A));
+    return 0;
+}
