af49f9b0da0afa508fa3573e8007e9b697896239
nizi <failaforcontact@gmail.com>
2021-02-04 10:15:24 +0900

    abc116/cを追加

---
+++ ABCPastQuestions/abc116/c/main.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+using lint = long long;
+typedef vector<long long> vint;
+typedef pair<long long, long long> pint;
+#define MD 1000000007
+#define INF INT32_MAX / 2
+#define INF64 INT64_MAX / 2
+#define EPS 0.001
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define all(x) (x).begin(),(x).end()
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(ll i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(ll i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
+
+
+void solve(long long N, std::vector<long long> h) {
+    ll ans = 0;
+    while (true) {
+        ll begin = -1;
+        ll end = -1;
+        REP (i, N) {
+            if (h[i] != 0 && begin == -1) begin = i;
+            if (h[i] == 0 && begin != -1) end = i - 1;
+            else if (i == N - 1 && end == -1) end = i;
+            if (end != -1) break;
+        }
+        if (begin == -1) {
+            c(ans)
+            return;
+        }
+        REP (i, end - begin + 1) {
+            h[begin + i]--;
+            // cout << "i=" << i << " " << h[i] << endl;
+        }
+        ans++;
+    }
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> h(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&h[i]);
+    }
+    solve(N, std::move(h));
+    return 0;
+}
