4556b018e5910c01fd083146162013f5256adada
nizi <failaforcontact@gmail.com>
2021-02-12 20:45:23 +0900

    abc089/cを追加

---
+++ ABCPastQuestions/abc089/c/main.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+using lint = long long;
+typedef vector<long long> vint;
+typedef pair<long long, long long> pint;
+#define MD 1000000007
+#define INF INT32_MAX / 2
+#define INF64 INT64_MAX / 2
+#define EPS 0.001
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define all(x) (x).begin(),(x).end()
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(ll i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(ll i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
+
+
+void solve(long long N, std::vector<std::string> S){
+    map<char, ll> mp;
+    vector<char> vec = {'M', 'A', 'R', 'C', 'H'};
+    REP (i, N) {
+        char c = S[i][0];
+        if (c == 'M') mp['M']++;
+        else if (c == 'A') mp['A']++;
+        else if (c == 'R') mp['R']++;
+        else if (c == 'C') mp['C']++;
+        else if (c == 'H') mp['H']++;
+    }
+    ll ans = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            for (int k = j + 1; k < 5; k++) {
+                // c(m.first << " " << mm.first << " " << M.first)
+                ans += mp[vec[i]] * mp[vec[j]] * mp[vec[k]];
+            }
+        }
+    }
+    c(ans)
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    std::vector<std::string> S(N);
+    for(int i = 0 ; i < N ; i++){
+        std::cin >> S[i];
+    }
+    solve(N, std::move(S));
+    return 0;
+}
