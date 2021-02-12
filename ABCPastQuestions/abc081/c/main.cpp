e28bf670de1b99b9959ac82cf7b5f7919dbe317d
nizi <failaforcontact@gmail.com>
2021-02-12 20:26:57 +0900

    abc081/cを追加

---
+++ ABCPastQuestions/abc081/c/main.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+
+void solve(long long N, long long K, std::vector<long long> A){
+    vector<long long> vec(N+1, 0);
+    long long kind = 0;
+    REP (i, N) {
+        if (!vec[A[i]]) kind++;
+        vec[A[i]]++;
+    }
+    ALL(sort, vec);
+    long long ans = 0;
+    REP (i, N) {
+        if (kind <= K) break;
+        if (vec[i] != 0) {
+            ans += vec[i];
+            kind--;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    long long K;
+    scanf("%lld",&K);
+    std::vector<long long> A(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&A[i]);
+    }
+    solve(N, K, std::move(A));
+    return 0;
+}
