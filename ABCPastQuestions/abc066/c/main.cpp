ac943dd4047a9a122db3680d9b9938276de9b82a
nizi <failaforcontact@gmail.com>
2021-02-12 23:01:18 +0900

    abc066/cを追加

---
+++ ABCPastQuestions/abc066/c/main.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+using lint = long long;
+typedef vector<long long> vint;
+typedef pair<long long, long long> pint;
+#define MD 1000000007
+#define INF INT32_MAX / 2
+#define INF64 INT64_MAX / 2
+#define EPS 0.001
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define all(x) (x).begin(),(x).end()
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(ll i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(ll i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
+
+
+void solve(long long n, std::vector<long long> a){
+    deque<long long> deq;
+    REP (i, n) {
+        if (n % 2 == 0) {
+            if (i % 2 != 0) deq.push_front(a[i]);
+            else deq.push_back(a[i]);
+        } else {
+            if (i % 2 == 0) deq.push_front(a[i]);
+            else deq.push_back(a[i]);
+        }
+    }
+    REP (i, n) {
+        if (i != n - 1) cout << deq.front() << " ";
+        else cout << deq.front();
+        deq.pop_front();
+    }
+    c("")
+}
+
+int main(){
+    long long n;
+    scanf("%lld",&n);
+    std::vector<long long> a(n);
+    for(int i = 0 ; i < n ; i++){
+        scanf("%lld",&a[i]);
+    }
+    solve(n, std::move(a));
+    return 0;
+}
