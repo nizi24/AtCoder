7336ec4e36647677fd23174ee71f21e521b5a10b
nizi <failaforcontact@gmail.com>
2021-02-12 22:40:58 +0900

    abc069/cを追加

---
+++ ABCPastQuestions/abc069/c/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+using lint = long long;
+typedef vector<long long> vint;
+typedef pair<long long, long long> pint;
+#define MD 1000000007
+#define INF INT32_MAX / 2
+#define INF64 INT64_MAX / 2
+#define EPS 0.001
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define all(x) (x).begin(),(x).end()
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(ll i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(ll i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
+
+const string YES = "Yes";
+const string NO = "No";
+
+void solve(long long N, std::vector<long long> a){
+    ll cnt = 0;
+    bool n = N % 2 != 0;
+    REP (i, N) {
+        if (a[i] % 4 == 0 && n) {
+            cnt += 3;
+            n = 0;
+        } else if (a[i] % 4 == 0) cnt += 2;
+        else if (a[i] % 2 == 0) cnt += 1;
+    }
+    if (cnt >= N) c(YES)
+    else c(NO)
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> a(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&a[i]);
+    }
+    solve(N, std::move(a));
+    return 0;
+}
