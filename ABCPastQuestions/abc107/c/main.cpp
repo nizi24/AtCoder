7a64cfe73b278e26ea8e7f6ce297d2b6b787abc1
nizi <failaforcontact@gmail.com>
2021-02-14 21:00:34 +0900

    abc107/cを改善

--- ABCPastQuestions/abc107/c/main.cpp
+++ ABCPastQuestions/abc107/c/main.cpp
@@ -24,28 +24,10 @@ bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!
 
 
 void solve(long long N, long long K, std::vector<long long> x){
-    // vint pos;
-    // vint neg(N);
-    // bool zero = 0;
-    // REP (i, N) {
-    //     if (x[i] > 0) pos.push_back(x[i]);
-    //     else if (x[i] < 0) neg.push_back(x[i]);
-    //     else zero = 1;
-    // }
-    // ll pos_size = pos.size();
-    // ll neg_size = neg.size();
     ll mn = INF64;
     REP (i, N-K+1) {
-        if (x[i] <= 0 && x[i+K-1] <= 0) {
-            chmin(mn, abs(x[i]));
-        } else if (x[i] >= 0 && x[i+K-1] >= 0) {
-            chmin(mn, abs(x[i+K-1]));
-        } else {
-            ll MN = min(abs(x[i]), abs(x[i+K-1]));
-            ll MX = max(abs(x[i]), abs(x[i+K-1]));
-            chmin(mn, MN * 2 + MX);
-        }
-        // c(mn)
+        chmin(mn, abs(x[i]) + abs(x[i] - x[i+K-1]));
+        chmin(mn, abs(x[i+K-1]) + abs(x[i] - x[i+K-1]));
     }
     c(mn)
 }
