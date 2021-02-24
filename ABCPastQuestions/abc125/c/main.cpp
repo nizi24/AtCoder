7dda31856cffbdc98df45cef2213bd7a20130a34
nizi <failaforcontact@gmail.com>
2021-02-24 22:01:36 +0900

    abc125/cを追加

---
+++ ABCPastQuestions/abc125/c/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define all(x) (x).begin(),(x).end()
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(int i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(int i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+
+
+void solve(long long N, std::vector<long long> A){
+    vector<ll> left(N+1, 0), right(N+1, 0);
+    left[0] = 0;
+    right[N] = A[N - 1];
+    for (int i = 0; i < N; i++) left[i + 1] = gcd(A[i], left[i]);
+    for (int i = N - 1; i >= 0; i--) right[i] = gcd(A[i], right[i + 1]);
+    ll max = 1;
+    REP (i, N) {
+        if (i == 0) max = right[i + 1];
+        else if (i == N - 1) chmax(max, left[i]);
+        else chmax(max, gcd(left[i], right[i + 1]));
+    }
+    c(max)
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> A(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&A[i]);
+    }
+    solve(N, std::move(A));
+    return 0;
+}
