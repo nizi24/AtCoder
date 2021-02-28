7a62e4dbc3b2837408fe57569e929657157f00aa
nizi <failaforcontact@gmail.com>
2021-02-28 18:14:50 +0900

    solve agc034/a

---
+++ ABCPastQuestions/agc034/a/main.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+using lint = long long;
+using Graph = vector<vector<ll>>;
+typedef vector<long long> vint;
+typedef pair<long long, long long> pint;
+#define MD 1000000007
+#define INF INT32_MAX / 2
+#define INF64 INT64_MAX / 2
+#define EPS 0.001
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define all(x) (x).begin(),(x).end()
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(ll i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(ll i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
+
+const string YES = "Yes";
+const string NO = "No";
+
+void solve(long long N, long long A, long long B, long long C, long long D, std::string S){
+    REP (i, max(C, D)) {
+        if (i + 1 >= A && S[i] == '#' && S[i + 1] == '#') {
+            c(NO)
+            return;
+        }
+    }
+    bool snuke_can_over = false;
+        for (int i = B - 1; i < D; i++) {
+            if (S[i - 1] == '.' && S[i] == '.' && S[i + 1] == '.') {
+            snuke_can_over = true;
+        }
+    }
+    if (!snuke_can_over && C > D) {
+        c(NO)
+    } else {
+        c(YES)
+    }
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    long long A;
+    scanf("%lld",&A);
+    long long B;
+    scanf("%lld",&B);
+    long long C;
+    scanf("%lld",&C);
+    long long D;
+    scanf("%lld",&D);
+    std::string S;
+    std::cin >> S;
+    S.push_back(' ');
+    solve(N, A, B, C, D, S);
+    return 0;
+}
