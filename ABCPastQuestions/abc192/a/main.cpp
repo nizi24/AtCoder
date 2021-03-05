0b5e1263a30d0690d7c875eb9f2331fdd7f0bd83
nizi <failaforcontact@gmail.com>
2021-03-05 20:33:28 +0900

    solve abc192/a

--- ABCPastQuestions/abc192/a/main.cpp
+++ ABCPastQuestions/abc192/a/main.cpp
@@ -3,7 +3,6 @@ using namespace std;
 using i64 = int64_t;
 using ll = long long;
 using lint = long long;
-using Graph = vector<vector<ll>>;
 typedef vector<long long> vint;
 typedef pair<long long, long long> pint;
 #define MD 1000000007
@@ -23,49 +22,14 @@ bool is_prime(ll N){if(N<=1)return false;for(ll i=2;i*i<=N;i++){if(N%i==0) retur
 template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
 bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
 
-const long long MOD = 2;
 
-void solve(long long N, long long P, std::vector<long long> A){
-    ll odd = 0;
-    ll even = 0;
-    REP (i, N) {
-        if (A[i] % 2 == 0) even++;
-        else odd++;
-    }
-    ll ans = 0;
-    ll odd_cnt = 0;
-    if (P == 0) {
-        ans += pow(2, even);
-        odd_cnt = 2;
-    } else {
-        odd_cnt = 1;
-    }
-    while (odd_cnt <= N) {
-        __int128_t c = 1;
-        __int128_t C = 1;
-        REP (i, odd_cnt) {
-            c *= odd - i;
-            C *= odd_cnt - i;
-            if (c % C == 0) {
-                c /= C;
-                C = 1;
-            }
-        }
-        ans += c / C * pow(2, even);
-        odd_cnt += 2;
-    }
-    c(ans)
+void solve(long long X){
+    c((X % 100 == 0 ? 100 : 100 - X % 100))
 }
 
 int main(){
-    long long N;
-    scanf("%lld",&N);
-    long long P;
-    scanf("%lld",&P);
-    std::vector<long long> A(N);
-    for(int i = 0 ; i < N ; i++){
-        scanf("%lld",&A[i]);
-    }
-    solve(N, P, std::move(A));
+    long long X;
+    scanf("%lld",&X);
+    solve(X);
     return 0;
 }
