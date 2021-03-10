ec9230fe5503de00702212bb4aa3f1f237c3dea7
nizi <failaforcontact@gmail.com>
2021-03-10 16:53:38 +0900

    CODEFESTIVAL2017C/c solve

---
+++ ABCPastQuestions/CODEFESTIVAL2017qualC/c.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+using lint = long long;
+typedef vector<long long> vint;
+typedef pair<long long, long long> pint;
+#define MD 1000000007
+#define INF INT32_MAX / 2
+#define INF64 INT64_MAX / 2
+#define EPS 0.001
+#define EPS14  1.0E-14
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define all(x) (x).begin(),(x).end()
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(ll i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(ll i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
+
+int main() {
+    string s;
+    cin >> s;
+    string S;
+    REP (i, s.size()) if (s[i] != 'x') S.push_back(s[i]);
+    string ss = S;
+    ALL(reverse, ss);
+    ll N = s.size();
+    if (ss == S) {
+        ll r = 0;
+        ll l = 0;
+        ll cnt = 0;
+        while (l < N - r - 1) {
+            if (s[l] == s[N - r - 1]) {
+                r++;
+                l++;
+            } else {
+                if (s[l] == 'x') l++;
+                else r++;
+                cnt++;
+            }
+        }
+        c(cnt)
+    } else {
+        c(-1)
+    }
+
+    return 0;
+}
