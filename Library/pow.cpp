a505425932eeb9bf04f375dfad6d14788271a364
nizi <failaforcontact@gmail.com>
2021-03-13 14:20:57 +0900

    solve arc106/a

---
+++ Library/pow.cpp
@@ -0,0 +1,9 @@
+long long pw(long long x, long long n) {
+    long long ret = 1;
+    while (n > 0) {
+        if (n & 1) ret *= x;  // n の最下位bitが 1 ならば x^(2^i) をかける
+        x *= x;
+        n >>= 1;  // n を1bit 左にずらす
+    }
+    return ret;
+}
