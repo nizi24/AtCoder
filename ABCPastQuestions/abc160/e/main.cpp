4239cec8ac542be1c86b10131f818427cc4bde07
nizi <failaforcontact@gmail.com>
2021-03-15 11:07:25 +0900

    solve abc160/e

--- ABCPastQuestions/abc160/e/main.cpp
+++ ABCPastQuestions/abc160/e/main.cpp
@@ -24,28 +24,53 @@ template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
 ll pw(ll x, ll n){ll ret=1;while(n>0){if(n&1){ret*=x;}x *= x;n >>= 1;}return ret;}
 bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
 
-{% if mod %}
-const long long MOD = {{ mod }};
-{% endif %}
-{% if yes_str %}
-const string YES = "{{ yes_str }}";
-{% endif %}
-{% if no_str %}
-const string NO = "{{ no_str }}";
-{% endif %}
 
-{% if prediction_success %}
-void solve({{ formal_arguments }}){
+void solve(long long X, long long Y, long long A, long long B, long long C, std::vector<long long> p, std::vector<long long> q, std::vector<long long> r){
+    sort(rall(p));
+    sort(rall(q));
+    sort(rall(r));
+    priority_queue<ll, vector<ll>, greater<ll>> que;
+    REP (i, X) que.push(p[i]);
+    REP (i, Y) que.push(q[i]);
 
+    ll ans = 0;    
+    ll cnt = 0;
+    REP (i, X + Y) {
+        if (que.top() < r[cnt]) {
+            ans += r[cnt];
+            cnt++;
+        } else {
+            ans += que.top();
+        }
+        que.pop();
+    }
+
+    c(ans)
 }
-{% endif %}
 
 int main(){
-    {% if prediction_success %}
-    {{input_part}}
-    solve({{ actual_arguments }});
-    {% else %}
-    // Failed to predict input format
-    {% endif %}
+    long long X;
+    scanf("%lld",&X);
+    long long Y;
+    scanf("%lld",&Y);
+    long long A;
+    scanf("%lld",&A);
+    long long B;
+    scanf("%lld",&B);
+    long long C;
+    scanf("%lld",&C);
+    std::vector<long long> p(A);
+    for(int i = 0 ; i < A ; i++){
+        scanf("%lld",&p[i]);
+    }
+    std::vector<long long> q(B);
+    for(int i = 0 ; i < B ; i++){
+        scanf("%lld",&q[i]);
+    }
+    std::vector<long long> r(C);
+    for(int i = 0 ; i < C ; i++){
+        scanf("%lld",&r[i]);
+    }
+    solve(X, Y, A, B, C, std::move(p), std::move(q), std::move(r));
     return 0;
 }
