e346869b5ae66c42a0dd437059af07bc987edd5e
nizi <failaforcontact@gmail.com>
2021-03-15 11:36:00 +0900

    solve arc067/d

--- ABCPastQuestions/arc067/d/main.cpp
+++ ABCPastQuestions/arc067/d/main.cpp
@@ -5,13 +5,12 @@ using ll = long long;
 using lint = long long;
 typedef vector<long long> vint;
 typedef pair<long long, long long> pint;
+#define MD 1000000007
 #define INF INT32_MAX / 2
 #define INF64 INT64_MAX / 2
 #define EPS 0.001
-#define EPS14  1.0E-14
-#define REP(i, n) for (ll i = 0; i < n; i++)
+#define REP(i, n) for (int i = 0; i < n; i++)
 #define all(x) (x).begin(),(x).end()
-#define rall(x) (x).rbegin(),(x).rend()
 #define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
 #define c(n) cout<<n<<endl;
 #define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
@@ -21,56 +20,28 @@ template<class T>inline T sum(T n){return n*(n+1)/2;}
 map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(ll i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
 bool is_prime(ll N){if(N<=1)return false;for(ll i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
 template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
-ll pw(ll x, ll n){ll ret=1;while(n>0){if(n&1){ret*=x;}x *= x;n >>= 1;}return ret;}
 bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
 
 
-void solve(long long X, long long Y, long long A, long long B, long long C, std::vector<long long> p, std::vector<long long> q, std::vector<long long> r){
-    sort(rall(p));
-    sort(rall(q));
-    sort(rall(r));
-    priority_queue<ll, vector<ll>, greater<ll>> que;
-    REP (i, X) que.push(p[i]);
-    REP (i, Y) que.push(q[i]);
-
+void solve(long long N, long long A, long long B, std::vector<long long> X){
     ll ans = 0;
-    ll cnt = 0;
-    REP (i, X + Y) {
-        if (que.top() < r[cnt]) {
-            ans += r[cnt];
-            cnt++;
-        } else {
-            ans += que.top();
-        }
-        que.pop();
+    REP (i, N-1) {
+        ans += min((X[i + 1] - X[i]) * A, B);
     }
-
     c(ans)
 }
 
 int main(){
-    long long X;
-    scanf("%lld",&X);
-    long long Y;
-    scanf("%lld",&Y);
+    long long N;
+    scanf("%lld",&N);
     long long A;
     scanf("%lld",&A);
     long long B;
     scanf("%lld",&B);
-    long long C;
-    scanf("%lld",&C);
-    std::vector<long long> p(A);
-    for(int i = 0 ; i < A ; i++){
-        scanf("%lld",&p[i]);
+    std::vector<long long> X(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&X[i]);
     }
-    std::vector<long long> q(B);
-    for(int i = 0 ; i < B ; i++){
-        scanf("%lld",&q[i]);
-    }
-    std::vector<long long> r(C);
-    for(int i = 0 ; i < C ; i++){
-        scanf("%lld",&r[i]);
-    }
-    solve(X, Y, A, B, C, std::move(p), std::move(q), std::move(r));
+    solve(N, A, B, std::move(X));
     return 0;
 }
