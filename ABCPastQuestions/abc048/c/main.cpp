9e39cadda425f4a7ec987e749e9ef4efea776f0a
nizi <failaforcontact@gmail.com>
2021-03-22 20:50:36 +0900

    solve abc048/c

---
+++ ABCPastQuestions/abc048/c/main.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+using lint = long long;
+typedef vector<long long> vint;
+typedef pair<long long, long long> pint;
+#define MD 1000000007
+#define INF INT32_MAX / 2
+#define INF64 INT64_MAX / 2
+#define EPS 0.001
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define all(x) (x).begin(),(x).end()
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(ll i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(ll i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
+
+
+void solve(long long N, long long x, std::vector<long long> a){
+    ll ans = 0;
+    REP (i, N-1) {
+        if (a[i] + a[i + 1] > x) {
+            if (a[i] <= x) {
+                ll tmp = a[i + 1];
+                a[i + 1] = x - a[i];
+                ans += tmp - a[i + 1];
+            } else {
+                ans += a[i + 1];
+                ans += a[i] - x;
+                a[i + 1] = 0;
+            }
+        }
+    }
+
+    c(ans)
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    long long x;
+    scanf("%lld",&x);
+    std::vector<long long> a(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&a[i]);
+    }
+    solve(N, x, std::move(a));
+    return 0;
+}
