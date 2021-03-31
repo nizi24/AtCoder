7e7bf046af55acf0d4431615aa89c90d1296ed24
nizi <failaforcontact@gmail.com>
2021-03-31 18:01:13 +0900

    solve agc014/b

---
+++ ABCPastQuestions/agc014/b/main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+using lint = long long;
+typedef vector<long long> vint;
+typedef pair<long long, long long> pint;
+#define INF INT32_MAX / 2
+#define INF64 INT64_MAX / 2
+#define EPS 0.001
+#define EPS14  1.0E-14
+#define REP(i, n) for (ll i = 0; i < n; i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(ll i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(ll i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+ll pw(ll x, ll n){ll ret=1;while(n>0){if(n&1){ret*=x;}x *= x;n >>= 1;}return ret;}
+bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
+
+const string YES = "YES";
+const string NO = "NO";
+
+void solve(long long N, long long M, std::vector<long long> a, std::vector<long long> b) {
+    vector<ll> cnt(N+1, 0);
+    REP (i, M) {
+        cnt[a[i]]++;
+        cnt[b[i]]++;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if (cnt[i] % 2 != 0) {
+            c(NO)
+            return;
+        }
+    }
+
+    c(YES)
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    long long M;
+    scanf("%lld",&M);
+    std::vector<long long> a(M);
+    std::vector<long long> b(M);
+    for(int i = 0 ; i < M ; i++){
+        scanf("%lld",&a[i]);
+        scanf("%lld",&b[i]);
+    }
+    solve(N, M, std::move(a), std::move(b));
+    return 0;
+}
