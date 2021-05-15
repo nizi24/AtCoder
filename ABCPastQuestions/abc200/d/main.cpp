9c8166fdac20f70b184618f5fc9f996bafcab442
nizi <failaforcontact@gmail.com>
2021-05-15 15:45:37 +0900

    solved abc200/d

---
+++ ABCPastQuestions/abc200/d/main.cpp
@@ -0,0 +1,295 @@
+#ifndef INCLUDED_MAIN
+#define INCLUDED_MAIN
+
+#include __FILE__ 
+
+const string YES = "Yes";
+const string NO = "No";
+
+void rec(const vector<vector<long long>> &dp, const vector<ll> &a, int i, int j, deque<int> &keiro, vector<deque<int>> &ans) {
+    if (i == 0) {
+        if (j == 0) {
+            ans.push_back(keiro);
+        }
+        return;
+    }
+
+    if (ans.size() >= 2) return;
+
+    // 上へ遡る (dp[i-1][j] == 0) だったら無視)
+    if (dp[i-1][j]) {
+        rec(dp, a, i-1, j, keiro, ans);
+    }
+    // 左上へ遡る (dp[i-1][j-a[i-1]] == 0 だったら無視)
+    if (j-a[i-1] >= 0 && dp[i-1][j-a[i-1]]) {
+        keiro.push_front(i);
+        rec(dp, a, i-1, j-a[i-1], keiro, ans);
+        keiro.pop_front();
+    }
+}
+void solve(long long N, std::vector<long long> A) {
+    queue<ll> que;
+    REP (i, N) {
+        A[i] %= 200;
+        if (A[i] % 200 == 0) que.push(i + 1);
+    }
+
+    if (que.size() >= 2) {
+        c(YES)
+        c(1 << " " << que.front())
+        que.pop();
+        c(1 << " " << que.front())
+        return;
+    }
+
+    vector<vector<ll>> dp(210, vector<ll>(21000, 0));
+    dp[0][0] = 1;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j <= 21000; ++j) {
+            (dp[i+1][j] += dp[i][j]) %= 200;
+            if (j >= A[i]) (dp[i+1][j] += dp[i][j-A[i]]) %= 200;
+        }
+    }
+
+    vector<vector<deque<int>>> ans(210);
+    for (int k = 1; k < 20010; k++) {
+
+        deque<int> keiro;
+        vector<deque<int>> a;
+        rec(dp, A, N, k, keiro, a);
+
+        bool ok = 0;
+        REP (i, a.size()) {
+            ans[k % 200].push_back(a[i]);
+
+            if (ans[k % 200].size() >= 2) {
+                ok = 1;
+                break;
+            }
+        }
+
+        if (ok) break;
+    }
+
+    REP (i, 200) {
+
+        if (ans[i].size() >= 2) {
+            c(YES)
+            cout << ans[i][0].size() << " ";
+            vector<int> cur;
+            while (!ans[i][0].empty()) {
+                cur.push_back(ans[i][0].front());
+                ans[i][0].pop_front();
+            }
+            ALL(sort, cur);
+            REP (i, cur.size()) cout << cur[i] << " ";
+            cout << endl;
+
+            cout << ans[i][1].size() << " ";
+            cur.clear();
+            while (!ans[i][1].empty()) {
+                cur.push_back(ans[i][1].front());
+                ans[i][1].pop_front();
+            }
+            ALL(sort, cur);
+            REP (i, cur.size()) cout << cur[i] << " ";
+            cout << endl;
+
+            return;
+        }
+    }
+
+    c(NO)
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> A(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&A[i]);
+    }
+    solve(N, std::move(A));
+    return 0;
+}
+
+#else  // INCLUDED_MAIN
+
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+using lint = long long;
+typedef vector<long long> vint;
+typedef pair<long long, long long> pint;
+#define INF INT32_MAX / 2
+#define INF64 INT64_MAX / 2
+#define EPS 0.001
+#define EPS14  1.0E-14
+#define REP(i, n) for (ll i = 0; i < n; i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(ll i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(ll i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+ll pw(ll x, ll n){ll ret=1;while(n>0){if(n&1){ret*=x;}x *= x;n >>= 1;}return ret;}
+bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
+
+// modint: mod 計算を int を扱うように扱える構造体
+template<int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if (val < 0) val += MOD;
+    }
+    constexpr int getmod() { return MOD; }
+    constexpr Fp operator - () const noexcept {
+        return val ? MOD - val : 0;
+    }
+    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator += (const Fp& r) noexcept {
+        val += r.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp& operator -= (const Fp& r) noexcept {
+        val -= r.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp& operator *= (const Fp& r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp& operator /= (const Fp& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b; swap(a, b);
+            u -= t * v; swap(u, v);
+        }
+        val = val * u % MOD;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator == (const Fp& r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator != (const Fp& r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+        if (n == 0) return 1;
+        auto t = modpow(a, n / 2);
+        t = t * t;
+        if (n & 1) t = t * a;
+        return t;
+    }
+};
+
+#define MD 1000000007
+using mint = Fp<MD>;
+
+// 二項係数
+const int MAX = 510000; // 問題ごとに変更する
+const int MOD = 1000000007; // 問題ごとに変更する
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+/* encode: ランレングス圧縮を行う
+*/
+vector<pair<char, long long>> encode(const string& str) {
+    int n = (long long)str.size();
+    vector<pair<char, long long>> ret;
+    for (long long l = 0; l < n;) {
+        int r = l + 1;
+        for (; r < n && str[l] == str[r]; r++) {};
+        ret.push_back({str[l], r - l});
+        l = r;
+    }
+    return ret;
+}
+/* decode: ランレングス圧縮の復元を行う
+*/
+string decode(const vector<pair<char, long long>>& code) {
+    string ret = "";
+    for (auto p : code) {
+        for (long long i = 0; i < p.second; i++) {
+            ret.push_back(p.first);
+        }
+    }
+    return ret;
+}
+
+struct Edge {
+    int to; // 隣接頂点番号
+    long long w; // 重み
+    Edge(int to, long long w) : to(to), w(w) {}
+};
+
+using Graph = vector<vector<Edge>>;
+
+// UnionFied構造体
+struct UnionFind {
+    vector<long long> par, siz;
+
+    UnionFind(long long n) : par(n, -1) , siz(n, 1) { }
+
+    // 根を求める
+    long long root(long long x) {
+        if (par[x] == -1) return x;
+        else return par[x] = root(par[x]);
+    }
+
+    // x と y が同じグループに属するかどうか (根が一致するかどうか)
+    bool issame(long long x, long long y) {
+        return root(x) == root(y);
+    }
+
+    // x を含むグループと y を含むグループとを併合する
+    bool unite(long long x, long long y) {
+        x = root(x), y = root(y);
+        if (x == y) return false; 
+        if (siz[x] < siz[y]) swap(x, y);
+        par[y] = x;
+        siz[x] += siz[y];
+        return true;
+    }
+
+    // x を含むグループのサイズ
+    long long size(long long x) {
+        return siz[root(x)];
+    }
+};
+
+#endif  // INCLUDED_MAIN
