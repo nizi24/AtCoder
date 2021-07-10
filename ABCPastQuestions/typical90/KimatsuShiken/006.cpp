8a43afade48e6d4a7926e0ae4e6bb968e0867a5a
nizi <failaforcontact@gmail.com>
2021-07-10 17:58:20 +0900

    solved kimatsushiken/006

--- ABCPastQuestions/typical90/KimatsuShiken/006.cpp
+++ ABCPastQuestions/typical90/KimatsuShiken/006.cpp
@@ -24,22 +24,108 @@ template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
 ll pw(ll x, ll n){ll ret=1;while(n>0){if(n&1){ret*=x;}x *= x;n >>= 1;}return ret;}
 bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
 
+// modint: mod 計算を int を扱うように扱える構造体
+template<int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if (val < 0) val += MOD;
+    }
+    constexpr int getmod() { return MOD; }
+    constexpr Fp operator - () const noexcept {
+        return val ? MOD - val : 0;
+    }
+    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator += (const Fp& r) noexcept {
+        val += r.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp& operator -= (const Fp& r) noexcept {
+        val -= r.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp& operator *= (const Fp& r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp& operator /= (const Fp& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b; swap(a, b);
+            u -= t * v; swap(u, v);
+        }
+        val = val * u % MOD;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator == (const Fp& r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator != (const Fp& r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+        if (n == 0) return 1;
+        auto t = modpow(a, n / 2);
+        t = t * t;
+        if (n & 1) t = t * a;
+        return t;
+    }
+};
+
+#define MD 1000000007
+using mint = Fp<MD>;
+
+// 二項係数
+const int MAX = 510000; // 問題ごとに変更する
+const int MOD = 1000000007; // 問題ごとに変更する
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
 int main() {
     ll N, M;
     cin >> N >> M;
     vector<ll> t(M), x(M), y(M);
     REP (i, M) cin >> t[i] >> x[i] >> y[i];
 
-    vector<ll> X(M), Y(M);
+    
+    COMinit();
+    mint ans = COM(N*2, N);
+    ans *= N*2;
 
     REP (i, M) {
-        X[i] = x[i];
-        Y[i] = y[i];
-        if (t[i] == 1) X[i]++;
-        else Y[i]++;
+        if (t[i] == 1) ans -= (COM(x[i] + y[i], x[i]) * COM((N-x[i]-1) + (N-y[i]), N-y[i]));
+        else ans -= (COM(x[i] + y[i], x[i]) * COM((N-x[i]) + (N-y[i]-1), N-x[i]));
     }
 
-    ALL(sort, X); ALL(sort, Y);
+    c(ans)
 
-    
+
 }
