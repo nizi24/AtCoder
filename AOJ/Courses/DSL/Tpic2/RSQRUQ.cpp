ca9a3ee5043477847e881935de117cd3ab38759b
nizi <failaforcontact@gmail.com>
2021-07-15 09:04:48 +0900

    solved AOJ/RSQRUQ

--- AOJ/Courses/DSL/Tpic2/RSQRUQ.cpp
+++ AOJ/Courses/DSL/Tpic2/RSQRUQ.cpp
@@ -1,32 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct RSQ_RAQ_LazySegTree {
+long long INF = (1LL<<31)-1;
+
+struct RMQ_RUQ_LazySegTree {
     int n; // 葉の数
     vector<long long> node; // 完全2分木
     vector<long long> lazy; // 遅延配列
 
     // 初期化
-    RSQ_RAQ_LazySegTree(vector<long long> v) {
+    RMQ_RUQ_LazySegTree(vector<long long> v) {
         int sz = v.size();
         n = 1;
         while (n < sz) n *= 2; // 与えられた数列の項数以上の2^n個、葉を作る
-        node.resize(n*2-1, 0); // 0で初期化
-        lazy.resize(n*2-1, 0); // 遅延配列も初期化
+        node.resize(n*2-1,  0); // 0で初期化
+        lazy.resize(n*2-1, INF); // 遅延配列はINFで初期化
 
         for (int i = 0; i < sz; i++) node[n-1+i] = v[i]; // 葉を初期化
-        // 下から順に葉以外のnodeを初期化
-        for (int i = n-2; i >= 0; i--) node[i] = node[i*2+1] + node[i*2+2];
+        for (int i = n-2; i >= 0; i--) node[i] = node[i*2+1] + node[i*2+2]; // 下から順に葉以外のnodeを初期化
     }
 
     void eval(int k) {
-        if (lazy[k]) { // lazy[k]が0でない場合、伝播&更新の反映
-            node[k] += lazy[k];
+        if (lazy[k] != INF) { // lazy[k]がINFでない場合、伝播&更新の反映
+            node[k] = lazy[k];
             if (k < n-1) { // 子に伝搬
-                lazy[k*2+1] += lazy[k]/2;
-                lazy[k*2+2] += lazy[k]/2;
+                lazy[k*2+1] = lazy[k]/2;
+                lazy[k*2+2] = lazy[k]/2;
             }
-            lazy[k] = 0;
+            lazy[k] = INF;
         }
     }
 
@@ -49,19 +50,19 @@ struct RSQ_RAQ_LazySegTree {
 
     // [s, t)にxを加算
     // 再帰的に更新するために呼び出す側を別の関数に
-    void add(int s, int t, long long x) { add_query(s, t, 0, n, 0, x); }
+    void update(int s, int t, long long x) { update_query(s, t, 0, n, 0, x); }
 
-    void add_query(int s, int t, int l, int r, int n, long long x) {
+    void update_query(int s, int t, int l, int r, int n, long long x) {
         eval(n);
         if (r <= s || t <= l) return; // 範囲外なら終了
         // [s, t)が[l, r)を内包しているとき
         else if (s <= l && t >= r) {
-            lazy[n] += (r - l) * x; // 子にもつ葉の数だけ加算
+            lazy[n] = (r - l) * x; // 子にもつ葉の数 * x に変更する
             eval(n);
         } else {
             // (r+l)/2は区間の中心, 区間の中心を左端にするか、右端にするかで分岐する
-            add_query(s, t, l, (r+l)/2, n*2+1, x); // 左下の子を更新
-            add_query(s, t, (r+l)/2, r, n*2+2, x);  // 右下の子を更新
+            update_query(s, t, l, (r+l)/2, n*2+1, x); // 左下の子を更新
+            update_query(s, t, (r+l)/2, r, n*2+2, x);  // 右下の子を更新
             node[n] = node[n*2+1] + node[n*2+2];
         }
     }
@@ -76,8 +77,8 @@ struct RSQ_RAQ_LazySegTree {
 int main() {
     int n, q;
     cin >> n >> q;
-    vector<long long> v(n,  0); // 0で初期化
-    auto segtree = RSQ_RAQ_LazySegTree(v);
+    vector<long long> v(n,  0); // 2^31-1で初期化
+    auto segtree = RMQ_RUQ_LazySegTree(v);
 
     for (int i = 0; i < q; i++) {
         int com;
@@ -85,13 +86,15 @@ int main() {
         if (com == 0) {
             int s, t, x;
             cin >> s >> t >> x;
-            segtree.add(s-1, t, x); // 0-index, 半開区間に直して呼び出し
+            segtree.update(s, t+1, x); // 半開区間に直して呼び出し
         } else {
             int s, t;
             cin >> s >> t;
             long long ans = 0;
-            segtree.sum(s-1, t, ans); // 0-index, 半開区間に直して呼び出し
+            segtree.sum(s, t+1, ans); // 半開区間に直して呼び出し
             cout << ans << endl;
         }
     }
+
+    // segtree.output();
 }
