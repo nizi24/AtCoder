568d2f368bebcf2960114f59a24a76fd79c1f4c7
nizi <failaforcontact@gmail.com>
2021-07-22 17:01:58 +0900

    solved abc160/c

---
+++ ABCPastQuestions/abc160/c/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template<class T>
+inline T sum(T n){return n*(n+1)/2;}
+
+
+void solve(long long K, long long N, std::vector<long long> A){
+    long long ans = A[N-1]-A[0];
+    for (int i = 1; i < N; i++) {
+        chmin(ans, K-A[i]+A[i-1]);
+    }
+    cout << ans << endl;
+}
+
+int main(){
+    long long K;
+    scanf("%lld",&K);
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> A(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&A[i]);
+    }
+    solve(K, N, std::move(A));
+    return 0;
+}
