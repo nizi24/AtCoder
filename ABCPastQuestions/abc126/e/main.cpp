a21171a966a5e279108e11f73b43974d5427335f
nizi <failaforcontact@gmail.com>
2021-09-17 17:10:33 +0900

    sovled abc126/e

---
+++ ABCPastQuestions/abc126/e/main.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+#define MOD 1000000007
+#define INF INT32_MAX / 2
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define all(x) (x).begin(),(x).end()
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define c(n) cout<<n<<endl;
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(int i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(int i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+
+// UnionFind構造体
+struct UnionFind {
+    vector<long long> par, siz;
+
+    UnionFind(long long n) : par(n, -1) , siz(n, 1) { }
+
+    // 根を求める
+    long long root(long long x) {
+        if (par[x] == -1) return x;
+        else return par[x] = root(par[x]);
+    }
+
+    // x と y が同じグループに属するかどうか (根が一致するかどうか)
+    bool issame(long long x, long long y) {
+        return root(x) == root(y);
+    }
+
+    // x を含むグループと y を含むグループとを併合する
+    bool unite(long long x, long long y) {
+        x = root(x), y = root(y);
+        if (x == y) return false; 
+        if (siz[x] < siz[y]) swap(x, y);
+        par[y] = x;
+        siz[x] += siz[y];
+        return true;
+    }
+
+    // x を含むグループのサイズ
+    long long size(long long x) {
+        return siz[root(x)];
+    }
+};
+
+void solve(long long N, long long M, std::vector<long long> X, std::vector<long long> Y, std::vector<long long> Z){
+    UnionFind Uf(N);
+    REP (i, M) Uf.unite(X[i]-1, Y[i]-1);
+
+    set<ll> st;
+    REP (i, N) st.insert(Uf.root(i));
+
+    c(st.size())
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    long long M;
+    scanf("%lld",&M);
+    std::vector<long long> X(M);
+    std::vector<long long> Y(M);
+    std::vector<long long> Z(M);
+    for(int i = 0 ; i < M ; i++){
+        scanf("%lld",&X[i]);
+        scanf("%lld",&Y[i]);
+        scanf("%lld",&Z[i]);
+    }
+    solve(N, M, std::move(X), std::move(Y), std::move(Z));
+    return 0;
+}
