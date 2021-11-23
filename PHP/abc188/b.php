b335a02818932daacea832d8132eb0d283a2568e
nizi <failaforcontact@gmail.com>
2021-11-23 17:43:11 +0900

    solved abc188/b with php

---
+++ PHP/abc188/b.php
@@ -0,0 +1,10 @@
+<?php
+$N = (int)fgets(STDIN);
+$A = array_map('intval', explode(' ', fgets(STDIN)));
+$B = array_map('intval', explode(' ', fgets(STDIN)));
+
+$tmp = 0;
+for ($i = 0; $i < $N; $i++) {
+    $tmp += $A[$i] * $B[$i];
+}
+print $tmp == 0 ? 'Yes' : 'No';
