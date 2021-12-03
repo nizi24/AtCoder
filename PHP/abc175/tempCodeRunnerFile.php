c3ed1792adfe000d5ae00e580cb1b9cf6701c810
nizi <failaforcontact@gmail.com>
2021-12-03 17:10:21 +0900

    solved abc175/b with php

---
+++ PHP/abc175/tempCodeRunnerFile.php
@@ -0,0 +1,15 @@
+<?php
+$N = (int)fgets(STDIN);
+$L = array_map('intval', explode(' ', fgets(STDIN)));
+
+$ans = 0;
+for ($i = 0; $i < $N; $i++) {
+    for ($j = $i + 1; $j < $N; $j++) {
+        for ($k = $j + 1; $k < $N; $k++) {
+            if ($L[$i] + $L[$j] > $L[$k] &&
+                $L[$i] + $L[$k] > $L[$j] &&
+                $L[$j] + $L[$k] > $L[$i]) $ans++;
+        }
+    }
+}
+print $ans;
