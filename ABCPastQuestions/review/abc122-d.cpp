9d77987ef9dc1478eccfea7667daca4f677e88a0
nizi <failaforcontact@gmail.com>
2021-12-17 21:47:02 +0900

    sovled abc122/d

---
+++ ABCPastQuestions/review/abc122-d.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using ll = long long;
+using lint = long long;
+typedef vector<long long> vint;
+typedef pair<long long, long long> pint;
+#define INF INT32_MAX / 2
+#define INF64 INT64_MAX / 2
+#define EPS 0.001
+#define EPS14  1.0E-14
+#define REP(i, n) for (ll i = 0; i < n; i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+#define cf(n) cout<<fixed<<setprecision(15)<<n<<endl;
+template <class T>inline bool chmin(T&a,T b) {if(a>b){a=b;return true;}return false;}
+template <class T>inline bool chmax(T&a,T b) {if(a<b){a=b;return true;}return false;}
+template<class T>inline T sum(T n){return n*(n+1)/2;}
+map<ll,ll> prime_fac(ll A) {map<ll,ll>mp;for(ll i=2;i*i<=A;i++){while(A%i== 0){mp[i]++;A/=i;}}if(A!=1){mp[A]=1;}return mp;}
+bool is_prime(ll N){if(N<=1)return false;for(ll i=2;i*i<=N;i++){if(N%i==0) return false;}return true;}
+template<class T>inline T myceil(T a,T b){return (a+(b-1))/b;}
+ll pw(ll x, ll n){ll ret=1;while(n>0){if(n&1){ret*=x;}x *= x;n >>= 1;}return ret;}
+bool is_product_overflow(long long a,long long b) {long prod=a*b;return (prod/b!=a);}
+
+// modint: mod 計算を int を扱うように扱える構造体
+template<int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if (val < 0) val += MOD;
+    }
+    constexpr int getmod() { return MOD; }
+    constexpr Fp operator - () const noexcept {
+        return val ? MOD - val : 0;
+    }
+    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator += (const Fp& r) noexcept {
+        val += r.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp& operator -= (const Fp& r) noexcept {
+        val -= r.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp& operator *= (const Fp& r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp& operator /= (const Fp& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b; swap(a, b);
+            u -= t * v; swap(u, v);
+        }
+        val = val * u % MOD;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator == (const Fp& r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator != (const Fp& r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+        if (n == 0) return 1;
+        auto t = modpow(a, n / 2);
+        t = t * t;
+        if (n & 1) t = t * a;
+        return t;
+    }
+};
+
+#define MD 1000000007
+using mint = Fp<MD>;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    // 0: A, 1: G, 2: C, 3: T
+    vector<vector<vector<vector<mint>>>> dp(N+1, vector<vector<vector<mint>>>(4,
+                                            vector<vector<mint>>(4, vector<mint>(4, 0))));
+    dp[0][3][3][3] = 1;
+
+    REP (i, N) REP (j, 4) REP (k, 4) REP (l, 4) REP (x, 4) {
+        // AGC
+        if (k == 0 && l == 1 && x == 2) continue;
+        // ACG
+        if (k == 0 && l == 2 && x == 1) continue;
+        // GAC
+        if (k == 1 && l == 0 && x == 2) continue;
+        // A?GC
+        if (j == 0 && l == 1 && x == 2) continue;
+        // AG?C
+        if (j == 0 && k == 1 && x == 2) continue;
+
+        dp[i + 1][k][l][x] += dp[i][j][k][l];
+    }
+
+    mint ans = 0;
+    REP (i, 4) REP (j, 4) REP (k, 4) ans += dp[N][i][j][k];
+    cout << ans << endl;
+
+    return 0;
+}
